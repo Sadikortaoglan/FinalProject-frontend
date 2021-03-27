a7a4520d20af665f6720beb7d3db3e3065d3c1f2
Onelife <ortcellad@gmail.com>
2021-03-27 23:24:13 +0300

    Api bağlantıları iyileştirildi

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginModel } from '../models/loginModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+import { TokenModel } from '../models/tokenModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiUrl = 'https://localhost:44361/api/auth/';
+  constructor(private httpClient:HttpClient) { }
+
+
+  login(LoginModel:LoginModel){
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login",LoginModel)
+  }
+
+
+isAuthenticated(){
+  if(localStorage.getItem("token")){
+    return true
+  }
+  else
+  {
+    return false;
+  }
+}
+}
